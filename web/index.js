f55dc5da29cbcfc517da98f74f86e0a45f26d13f
Gomiyux <carlosgomezcoca@gmail.com>
2016-12-22 18:21:07 +0100

    v.1.0.3
    Footer + Desplegable Jumbotron

---
+++ web/index.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function show(){
+    
+    
+    
+    if(document.getElementById("oculto_principal").style.display=="none"){
+    document.getElementById("oculto_principal").style.display="block";
+    }
+    else{
+    document.getElementById("oculto_principal").style.display="none";
+    }
+    
+    
+}
+
