f9445442bcbf00621add02008305c5d686bdcc43
Gomiyux <carlosgomezcoca@gmail.com>
2016-12-23 23:49:50 +0100

    v.1.0.4
    Menu desplegable añadido para pequeñas resoluciones

--- web/index.js
+++ web/index.js
@@ -4,17 +4,51 @@
  * and open the template in the editor.
  */
 
+
+
+/*MUESTRA EL CONTENIDO DE JUMBOTRON*/
 function show(){
     
-    
-    
+
     if(document.getElementById("oculto_principal").style.display=="none"){
     document.getElementById("oculto_principal").style.display="block";
+    document.getElementById("botonampli").textContent='Ver Menos';
+    document.getElementById("footer").style.position="relative";
+    
     }
     else{
     document.getElementById("oculto_principal").style.display="none";
+    document.getElementById("botonampli").textContent='Ver Más';
+    document.getElementById("footer").style.position="relative";
     }
-    
     
 }
 
+
+/*Desplegar Menu pantallas pequeñas*/
+function desplegar_menu(){
+    
+    if(document.getElementById("menu_desplegable").style.display=="none"){
+        
+        document.getElementById("menu_desplegable").style.display="block"
+        document.getElementById("footer").style.position="relative";    
+        
+        
+    }
+    else{
+        document.getElementById("menu_desplegable").style.display="none"
+        document.getElementById("footer").style.position="relative";
+        
+    }   
+}
+window.onresize = function() {
+    
+    
+    if (window.innerWidth >= 768) {  
+        
+        document.getElementById("menu_desplegable").style.display="none"
+        document.getElementById("footer").style.position="absolute";
+        
+    }
+    
+}
